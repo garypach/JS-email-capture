50df66ff781ddef0254513daf33dead269904606
Gary <garypachh@gmail.com>
2021-08-27 11:52:05 -0700

    update app.js
    added more functions to allow user to close modal and add thank you cont with set timeout property and cleaned up code

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
 window.onload = function(){
     let emailState =false;
     let emailModal = document.getElementsByClassName('email-modal')[0];
-    let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
+    let closeButton = document.getElementsByClassName('email-modal__close-btn')[0];
     let emailButton = document.getElementsByClassName('email-modal__button')[0];
     let emailInput = document.getElementsByClassName('email-modal__input')[0];
-
-    function ValidateEmail(mail) 
-    {
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)){
-            console.log(emailInput.value);
-            document.getElementsByClassName("email-modal__error-message")[0].classList.remove("email-modal__error-message--active")
-            document.getElementsByClassName("email-modal__form-group")[0].classList.remove("email-modal__form-group--error")
-            return (true)
-        }
+    let successCont = document.getElementsByClassName('email-thank')[0];
+    let declineOffer = document.getElementsByClassName('email-modal__decline-offer')[0];
+    let removeErr = ()=>{
+        document.getElementsByClassName("email-modal__error-message")[0].classList.remove("email-modal__error-message--active")
+        document.getElementsByClassName("email-modal__form-group")[0].classList.remove("email-modal__form-group--error")
+    }
+    let addErr = ()=>{
         document.getElementsByClassName("email-modal__error-message")[0].classList.add("email-modal__error-message--active")
         document.getElementsByClassName("email-modal__form-group")[0].classList.add("email-modal__form-group--error")
-
-        return (false)
     }
-    emailButton.addEventListener('click', ()=>{
-        ValidateEmail(emailInput.value);
-    })
+
     let showModal = ()=>{
         if(emailState==false){
             emailModal.classList.add("email-modal--visible");
             emailState = true;
         }
     }
+    let closeModal = ()=>{
+        emailModal.classList.remove("email-modal--visible");
+    }
+    let showSuccess = ()=>{
+        successCont.classList.add('email-thank--success');
+        setTimeout(()=>{
+            closeModal();
+        },3000);
+    }
+    function ValidateEmail(mail) 
+    {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)){
+            showSuccess();
+            return (true)
+        }
+        
+        addErr();
+        return (false)
+    }
+    emailButton.addEventListener('click', ()=>{
+        ValidateEmail(emailInput.value);
+    })
+
+    emailInput.addEventListener('click', ()=>{
+        removeErr();
+    })
 
     document.body.addEventListener('mouseleave',()=>{
         showModal();
     });
 
-    closeModal.addEventListener('click',()=>{
-        emailModal.classList.remove("email-modal--visible");
+    closeButton.addEventListener('click',()=>{
+        closeModal();
+    });
+
+    declineOffer.addEventListener('click',()=>{
+        closeModal();
     });
 }
